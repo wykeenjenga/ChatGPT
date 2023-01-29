e83602e0a0ba09aed5c995f32d03d1bd7f0830dd
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2023-01-29 11:02:07 +0300

    Initial Commit

---
+++ ChatGPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPT
+//
+//  Created by Wykee on 29/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChatGPTUITests/ChatGPTUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChatGPTUITestsLaunchTests.swift
+//  ChatGPTUITests
+//
+//  Created by Wykee on 29/01/2023.
+//
+
+import XCTest
+
+final class ChatGPTUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
